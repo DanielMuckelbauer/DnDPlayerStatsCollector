f074aeecbd069370c051aaded6140cb7a7c2e6c4
Daniel Muckelbauer <Daniel.Muckelbauer@L-P-A.com>
2020-12-04 13:27:22 +0100

    Error tests

--- Roll20Stats.Tests/PlayerStatisticTests/PlayerStatisticsControllerTests.cs
+++ Roll20Stats.Tests/PlayerStatisticTests/PlayerStatisticsControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -42,9 +43,21 @@ namespace Roll20Stats.Tests.PlayerStatisticTests
         }
 
         [Fact]
+        public async Task Returns_Not_Found_When_Statistic_Does_Not_Exist()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/api/playerstatistics/Id");
+
+            var responseObject = JsonConvert.DeserializeObject<ResponseWithMetaData<PlayerStatistic>>(await response.Content.ReadAsStringAsync());
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            responseObject.ErrorMessage.Should().Be(@"Player with id ""Id"" was not found.");
+        }
+
+        [Fact]
         public async Task Gets_All_PlayerStatistic()
         {
-            var stats = new []
+            var stats = new[]
             {
                 new PlayerStatistic
                 {
@@ -71,7 +84,7 @@ namespace Roll20Stats.Tests.PlayerStatisticTests
         [Fact]
         public async Task Creates_PlayerStatistic()
         {
-            var client = _factory.CreateDefaultClient();
+            var client = _factory.CreateClient();
             var request = new AddPlayerStatisticCommand
             {
                 CharacterId = "Id",
@@ -90,6 +103,43 @@ namespace Roll20Stats.Tests.PlayerStatisticTests
             var createResponseObject = JsonConvert.DeserializeObject<GetPlayerStatisticDto>(await response.Content.ReadAsStringAsync());
             getResponseObject.Should().BeEquivalentTo(request);
             createResponseObject.Should().BeEquivalentTo(request);
+        }
+
+        [Fact]
+        public async Task Adds_to_Existing_Player_Statistic()
+        {
+            var client = _factory.CreateClient();
+            var stat = new PlayerStatistic
+            {
+                CharacterId = "Id",
+                CharacterName = "Testosteron",
+                DamageDealt = 1,
+                DamageTaken = 2
+            };
+            TestDatabaseManager.SeedDatabase(_factory, stat);
+            var request = new AddPlayerStatisticCommand
+            {
+                CharacterId = "Id",
+                CharacterName = "Name",
+                DamageDealt = 1,
+                DamageTaken = 2
+            };
+            var requestBody = new StringContent(JsonConvert.SerializeObject(request));
+            requestBody.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = await client.PutAsync("/api/playerstatistics", requestBody);
+            var getResponse = await client.GetAsync("/api/playerstatistics/Id");
+
+            response.EnsureSuccessStatusCode();
+            var getResponseObject = JsonConvert.DeserializeObject<GetPlayerStatisticDto>(await getResponse.Content.ReadAsStringAsync());
+            getResponseObject.Should().BeEquivalentTo(new GetPlayerStatisticDto
+            {
+                CharacterId = "Id",
+                CharacterName = "Testosteron",
+                DamageDealt = 2,
+                DamageTaken = 4
+            });
+
         }
 
         [Fact]
